9f5b4414a8646389aacf9b3576849ec0a2f8569d
Andrey Pogorelov <namtydapro@gmail.com>
2019-12-17 04:17:33 +0300

    update build

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ and
 
 ```
 npm start
+```
+
+listen on
+
+http://localhost:3000/index
+
+http://localhost:3000/product
--- build/script.js
+++ build/script.js
@@ -1,7 +1,7 @@
 (function () {
     document.body.addEventListener('click', function (e) {
         if (e.target.classList.contains('onoffswitch') || e.target.classList.contains('onoffswitch__button')) {
-            let themedItems = document.querySelectorAll('.theme_color_project-default, .theme_color_project-inverse');
+            const themedItems = document.querySelectorAll('.theme_color_project-default, .theme_color_project-inverse');
             themedItems.forEach((themedItem) => {
                 if (themedItem.classList.contains('theme_color_project-default')) {
                     themedItem.classList.remove('theme_color_project-default');
@@ -15,10 +15,10 @@
     });
 
     document.body.addEventListener('click', function (e) {
-        let accordionShort = e.target.closest('.e-accordion__short');
+        const accordionShort = e.target.closest('.e-accordion__short');
         if (accordionShort && accordionShort.parentElement) {
 
-            let accordionMore = accordionShort.parentElement.querySelector('.e-accordion__more');
+            const accordionMore = accordionShort.parentElement.querySelector('.e-accordion__more');
             if (accordionMore) {
                 accordionMore.classList.toggle('e-accordion__more-visible')
             }
